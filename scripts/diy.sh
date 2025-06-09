8d12be33a4c73860a72de1c196fc996a78d253bd
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2025-06-09 11:22:02 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #更改默认地址为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.30/g' package/base-files/files/bin/config_generate
 

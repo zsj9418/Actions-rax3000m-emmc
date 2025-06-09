09a449d2de01f5987a469503c91a3e7661f05cec
zsj9418 <34057614+zsj9418@users.noreply.github.com>
2025-06-09 11:22:51 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -1,9 +1,9 @@
 #添加helloworld
-echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld.git' >> feeds.conf.default
+#echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #添加easymesh
-echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh.git' >> feeds.conf.default
+#echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh.git' >> feeds.conf.default
 #添加luci-app-adguardhome
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
+#echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
 # istore
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
